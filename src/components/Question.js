e2c145bf2e80ff098f60d804537a4d752d83384c
Andrew Wei <andrew.wei@skyscanner.net>
2019-09-24 18:21:00 +0800

    Refactor Question to a stateless component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
 
-class Question extends Component {
-    render() {
-        const { author, question, id} = this.props
+const Question = () => { // insert props as arguments here if any
+    const { author, question, id } = this.props
 
-        if (question === null) {
-            return <p>This question doesn't existd</p>
-        }
-
-        const {
-            name, avatarURL
-        } = author;
-
-        const {
-            optionOne
-        } = question
-
-        return (
-            
-                <div className='question'>
-                    <img
-                        src={avatarURL}
-                        alt={`Avatar of ${name}`}
-                        className='avatar'
-                    />
-                    <div className='question-info'>
-                        <h1>{name} asks:</h1>
-                        <h3>Would you rather</h3>
-                        <span>{optionOne.text}</span>
-                        <Link to={`/questions/${id}`}>
-                            <h5 className='btn'>
-                                View Poll
-                            </h5>
-                        </Link>
-                    </div>
-                </div>
-        )
+    if (question === null) {
+        return (<p>This question doesn't existd</p>)
     }
+
+    const {
+        name, avatarURL
+    } = author;
+
+    const {
+        optionOne
+    } = question
+
+    return (
+
+        <div className='question'>
+            <img
+                src={avatarURL}
+                alt={`Avatar of ${name}`}
+                className='avatar'
+            />
+            <div className='question-info'>
+                <h1>{name} asks:</h1>
+                <h3>Would you rather</h3>
+                <span>{optionOne.text}</span>
+                <Link to={`/questions/${id}`}>
+                    <h5 className='btn'>
+                        View Poll
+                            </h5>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 function mapStateToProps({ users, questions }, { id }) {
